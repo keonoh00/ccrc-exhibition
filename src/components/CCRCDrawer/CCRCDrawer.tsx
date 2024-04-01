a18164f9c41d154eac3cd395d6ce7847b817544b
keonoh00 <keonoh00@gmail.com>
2024-04-01 21:14:59 +0900

    Create common header for navigation

---
+++ src/components/CCRCDrawer/CCRCDrawer.tsx
@@ -0,0 +1,46 @@
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useRef } from "react";
+
+export default function CCRCDrawer() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  return (
+    <>
+      <HamburgerIcon w={10} h={10} ref={btnRef.current} onClick={onOpen} />
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef.current}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>This is left drawer</DrawerHeader>
+
+          <DrawerBody>
+            <p>Drawer body content</p>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button onClick={onClose} colorScheme="blue">
+              OK
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
