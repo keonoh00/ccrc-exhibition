042626ffd0355d99cc24ad9b73fbdace8675a8b1
keonoh00 <keonoh00@gmail.com>
2024-04-02 12:52:46 +0900

    Fix aspect ratio

--- src/components/ImageContainer/ImageContainer.tsx
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -3,8 +3,12 @@ import { AssetSelector } from "../../assets";
 
 export default function ImageContainer() {
   return (
-    <AspectRatio maxW="400px" ratio={4 / 3}>
-      <Image src={AssetSelector.systemModel} alt="naruto" objectFit="cover" />
+    <AspectRatio maxW="400px" ratio={7 / 6}>
+      <Image
+        src={AssetSelector.systemModel}
+        alt="systemmodel"
+        objectFit="cover"
+      />
     </AspectRatio>
   );
 }
