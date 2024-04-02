c56438bfc06bcbdc657f194b5ecab990af77dd19
keonoh00 <keonoh00@gmail.com>
2024-04-02 12:47:09 +0900

    Create Accordion component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,16 @@
+import CCRCAccordion from "../components/CCRCAccordian/CCRCAccordion";
 import ImageContainer from "../components/ImageContainer/ImageContainer";
 import { ScreenBaseProps } from "../types/screens";
 
 interface HomeScreenProps extends ScreenBaseProps {}
 
 const Home: React.FC<HomeScreenProps> = () => {
-  return <ImageContainer />;
+  return (
+    <>
+      <ImageContainer />
+      <CCRCAccordion />
+    </>
+  );
 };
 
 export default Home;
